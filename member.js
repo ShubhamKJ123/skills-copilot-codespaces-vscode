5c9f21b813c48478f7999dc7e30e6159df4517cc
Shubham Kumar Jha <122920531+ShubhamKJ123@users.noreply.github.com>
2023-07-22 09:11:07 +0000

    Copilot third commit

--- member.js
+++ member.js
@@ -1,4 +1,23 @@
-function skillsMember(){
-    console.log("I am a member");
-    
-}
+
+//create a web server
+
+//create a web server
+const server = http.createServer((req,res)=>{
+    console.log(req.url);
+    if(req.url === '/'){
+        res.write("Hello World");
+        res.end();
+    }
+    if(req.url === '/api/courses'){
+        res.write(JSON.stringify([1,2,3]));
+        res.end();
+    }
+    if(req.url === '/api/members'){
+        res.write(JSON.stringify(["member1","member2","member3"]));
+        res.end();
+    }
+});
+
+server.listen(3000);
+console.log("Listening on port 3000");
+
